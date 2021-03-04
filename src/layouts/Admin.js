0f672bf3797225c52d17201b7fba183fd683f8c9
benedict <kiokobenedict@gmail.com>
2021-03-04 18:37:17 +0300

    added an admin layout

---
+++ src/layouts/Admin.js
@@ -0,0 +1,36 @@
+import { Switch, Route, Redirect } from "react-router-dom";
+import React from "react";
+
+// components
+import Sidebar from "../components/Sidebar/Sidebar";
+import Footer from "../components/Footer/Footer";
+import Navbar from "../components/Navbar/Navbar";
+import HeaderStats from "../components/Header/HeaderStats";
+
+// views
+import Dashboard from "../pages/admin/Dashboard.js";
+import Target from "../pages/admin/Target.js";
+import Stats from "../pages/admin/Stats.js";
+
+export default function Admin() {
+  return (
+    <>
+      <Sidebar />
+      <div className="relative md:ml-64 bg-gray-200">
+        <Navbar />
+        {/* Header */}
+        <HeaderStats />
+        <div className="px-4 md:px-10 mx-auto w-full -m-24">
+          <Switch>
+            <Route path="/admin/dashboard" exact component={Dashboard} />
+            <Route path="/admin/target" exact component={Target} />
+            <Route path="/admin/stats" exact component={Stats} />
+
+            <Redirect from="/admin" to="/admin/dashboard" />
+          </Switch>
+          <Footer />
+        </div>
+      </div>
+    </>
+  );
+}
