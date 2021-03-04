9dc3648d7f151345cb1cd6154c9bcdda160dae93
benedict <kiokobenedict@gmail.com>
2021-03-04 18:36:25 +0300

    added user dropdowns

---
+++ src/components/Dropdowns/Notification.js
@@ -0,0 +1,73 @@
+import React from "react";
+import { createPopper } from "@popperjs/core";
+import { Link } from "react-router-dom";
+
+const Notification = () => {
+  // dropdown props
+  const [dropdownPopoverShow, setDropdownPopoverShow] = React.useState(false);
+  const btnDropdownRef = React.createRef();
+  const popoverDropdownRef = React.createRef();
+  const openDropdownPopover = () => {
+    console.log("hey");
+    createPopper(btnDropdownRef.current, popoverDropdownRef.current, {
+      placement: "bottom-start",
+    });
+    setDropdownPopoverShow(true);
+  };
+  const closeDropdownPopover = () => {
+    setDropdownPopoverShow(false);
+  };
+  return (
+    <>
+      <a
+        className="text-gray-600 block py-1 px-3"
+        href="#pablo"
+        ref={btnDropdownRef}
+        onClick={(e) => {
+          e.preventDefault();
+          dropdownPopoverShow ? closeDropdownPopover() : openDropdownPopover();
+        }}
+      >
+        <i className="fas fa-bell"></i>
+      </a>
+      <div
+        ref={popoverDropdownRef}
+        className={
+          (dropdownPopoverShow ? "block " : "hidden ") +
+          "bg-white text-base z-50 float-left py-2 list-none text-left rounded shadow-lg mt-1 min-w-48"
+        }
+      >
+        <Link
+          to="/profile"
+          className={
+            "text-sm py-2 px-4 font-normal block w-full whitespace-no-wrap bg-transparent text-gray-800"
+          }
+          onClick={(e) => e.preventDefault()}
+        >
+          Profile
+        </Link>
+        <Link
+          to=""
+          className={
+            "text-sm py-2 px-4 font-normal block w-full whitespace-no-wrap bg-transparent text-gray-800"
+          }
+          onClick={(e) => e.preventDefault()}
+        >
+          Settings
+        </Link>
+        <div className="h-0 my-2 border border-solid border-gray-200" />
+        <Link
+          to=""
+          className={
+            "text-sm py-2 px-4 font-normal block w-full whitespace-no-wrap bg-transparent text-gray-800"
+          }
+          onClick={(e) => e.preventDefault()}
+        >
+          Log Out
+        </Link>
+      </div>
+    </>
+  );
+};
+
+export default Notification;
