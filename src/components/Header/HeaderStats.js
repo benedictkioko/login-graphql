5313e53d34f2f3c91ce265bb635cc541817a0198
benedict <kiokobenedict@gmail.com>
2021-03-06 09:25:47 +0300

    updated the background gradient from right to top-right

--- src/components/Header/HeaderStats.js
+++ src/components/Header/HeaderStats.js
@@ -8,7 +8,7 @@ export default function HeaderStats() {
   return (
     <>
       {/* Header */}
-      <div className="relative bg-gradient-to-r from-gray-900 to-red-500 md:pt-32 pb-32 pt-12">
+      <div className="relative bg-gradient-to-tr from-gray-900 to-red-500 md:pt-32 pb-32 pt-12">
         <div className="px-4 md:px-10 mx-auto w-full">
           <div>
             {/* Card stats */}
