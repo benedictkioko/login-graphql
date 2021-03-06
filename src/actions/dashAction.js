9f7f986cc346332798734d0b8f28d8522583bc31
benedict <kiokobenedict@gmail.com>
2021-03-06 09:22:26 +0300

    added dashbaoard fetch and search items to the actions

--- src/actions/dashAction.js
+++ src/actions/dashAction.js
@@ -2,7 +2,15 @@ import { dashboard } from "./actionTypes";
 
 // target
 
-export const fetchTarget = (data) => ({
-  type: dashboard.GET_TARGETS,
-  payload: data,
-});
+export const fetchTarget = (data) => {
+  return {
+    type: dashboard.GET_TARGETS,
+    payload: data,
+  };
+};
+export const setSearchQuery = (data) => {
+  return {
+    type: dashboard.SET_SEARCH_QUERY,
+    payload: data,
+  };
+};
