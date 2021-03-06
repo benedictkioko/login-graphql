85b4eee4232d3ea1cbe6a317836e71eb1f835f4a
benedict <kiokobenedict@gmail.com>
2021-03-06 09:24:46 +0300

    modified the footer details and branding

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -7,20 +7,16 @@ export default function Footer() {
       <footer className="block py-4">
         <div className="container mx-auto px-4">
           <hr className="mb-4 border-b-1 border-gray-300" />
-          <div className="flex flex-wrap items-center md:justify-between justify-center">
-            <div className="w-full md:w-4/12 px-4">
-              <div className="text-sm text-gray-600 font-semibold py-1 text-center md:text-left">
-                Copyright © {new Date().getFullYear()}{" "}
-                <Link className="text-gray-600 hover:text-gray-800 text-sm font-semibold py-1">
-                  Hello Cyber
-                </Link>
-              </div>
-            </div>
+          <div className="flex flex-wrap items-center md:justify-between left-auto">
             <div className="w-full md:w-8/12 px-4">
               <ul className="flex flex-wrap list-none md:justify-end  justify-center">
                 <li>
-                  <Link className="text-gray-700 hover:text-gray-900 text-sm font-semibold block py-1 px-3">
-                    SCOT PLATFORM
+                  <Link
+                    to="/admin/dashboard"
+                    className="text-gray-700 hover:text-gray-900 text-sm font-semibold block py-1 px-3"
+                  >
+                    Copyright © {new Date().getFullYear()} Powered by SCOT
+                    Platform
                   </Link>
                 </li>
               </ul>
