1dd194bc60c3e037fc492ce5826c8640d2673c62
benedict <kiokobenedict@gmail.com>
2021-03-06 09:20:30 +0300

    added a search query reducer for the homepage search input

--- src/reducers/targetReducer.js
+++ src/reducers/targetReducer.js
@@ -2,10 +2,25 @@ import { dashboard } from "../actions/actionTypes";
 
 const initialState = {
   allTargets: [],
+  n: 50,
+  offset: 0,
+  search: "",
 };
 
 export default function targetReducer(state = initialState, action) {
   switch (action.type) {
+    case dashboard.SET_SEARCH_QUERY:
+      const newSearchState = {
+        ...state,
+        search: action.payload,
+      };
+      return newSearchState;
+    case dashboard.GET_TARGETS:
+      const newState = {
+        ...state,
+        ...action.payload,
+      };
+      return newState;
     case dashboard.TARGET_QUERY_SUCCESS:
       return {
         ...state,
