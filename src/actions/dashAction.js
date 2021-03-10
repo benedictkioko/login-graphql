f653815561e728a3690514990d4e8a4015b8824b
benedict <kiokobenedict@gmail.com>
2021-03-10 20:28:22 +0300

    added dastStats to the dash action

--- src/actions/dashAction.js
+++ src/actions/dashAction.js
@@ -1,13 +1,22 @@
 import { dashboard } from "./actionTypes";
 
-// target
+// dashboard stats
+export const dashStats = (data) => {
+  return {
+    type: dashboard.GET_STATS_SUCCESS,
+    payload: data,
+  };
+};
 
+// target
 export const fetchTarget = (data) => {
   return {
     type: dashboard.GET_TARGETS,
     payload: data,
   };
 };
+
+// search input
 export const setSearchQuery = (data) => {
   return {
     type: dashboard.SET_SEARCH_QUERY,
