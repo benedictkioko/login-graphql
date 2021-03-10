03cb6883e72008922d2aabdf859ff4cd3887b085
benedict <kiokobenedict@gmail.com>
2021-03-10 20:27:13 +0300

    updated the rootReducer with dashboard reducer

--- src/redux/rootReducer.js
+++ src/redux/rootReducer.js
@@ -1,11 +1,13 @@
 import { combineReducers } from "redux";
 import authReducer from "../reducers/authReducer";
 import targetReducer from "../reducers/targetReducer";
+import dashboardReducer from "../reducers/dashboardReducer";
 import storage from "redux-persist/lib/storage";
 
 const rootReducer = combineReducers({
   key: "root",
   auth: authReducer,
+  dashboard: dashboardReducer,
   targets: targetReducer,
   storage: storage,
 });
