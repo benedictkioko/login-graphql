b1e19b9e7df762a57cbd532971a3b10ee02bc2c5
benedict <kiokobenedict@gmail.com>
2021-03-11 21:13:40 +0300

    added a search results table component

---
+++ src/components/Tables/SearchResults/Body.js
@@ -0,0 +1,33 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+export default function Body({ data }) {
+  return (
+    <>
+      <tbody>
+        <tr
+          key={data.id}
+          className="px-6 py-4 whitespace-nowrap text-sm text-gray-500"
+        >
+          <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">
+            {data.name}{" "}
+          </td>{" "}
+          <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
+            {data.target}
+          </td>{" "}
+          <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
+            {data.ip}
+          </td>{" "}
+          <td className="px-6 py-4 whitespace-nowrap text-sm font-medium">
+            {data.status}
+          </td>
+          <td className="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
+            <Link to="" className="text-indigo-600 hover:text-indigo-900">
+              Edit
+            </Link>
+          </td>
+        </tr>
+      </tbody>
+    </>
+  );
+}
---
+++ src/components/Tables/SearchResults/Header.js
@@ -0,0 +1,39 @@
+import React from "react";
+
+export default function Header() {
+  return (
+    <>
+      <thead className="bg-white py-4">
+        <tr>
+          <th
+            scope="col"
+            className="px-6 py-3 text-left text-xs font-bold text-gray-900 uppercase tracking-wider"
+          >
+            Name
+          </th>
+          <th
+            scope="col"
+            className="px-6 py-3 text-left text-xs font-bold text-gray-900 uppercase tracking-wider"
+          >
+            Domain
+          </th>
+          <th
+            scope="col"
+            className="px-6 py-3 text-left text-xs font-bold text-gray-900 uppercase tracking-wider"
+          >
+            IP
+          </th>
+          <th
+            scope="col"
+            className="px-6 py-3 text-left text-xs font-bold text-gray-900 uppercase tracking-wider"
+          >
+            Status
+          </th>
+          <th scope="col" className="relative px-6 py-3">
+            <span className="sr-only">Edit</span>
+          </th>
+        </tr>
+      </thead>
+    </>
+  );
+}
