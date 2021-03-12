51196cf155c2c1e0d7a955b185c7fa9b8895922f
benedict <kiokobenedict@gmail.com>
2021-03-12 22:20:21 +0300

    layout setup for spectrum

--- src/pages/app/Spectrum.js
+++ src/pages/app/Spectrum.js
@@ -2,21 +2,12 @@ import React from "react";
 
 export default function Spectrum() {
   return (
-    <>
-      <div className="flex flex-wrap">
-        <div className="w-full xl:w-8/12 mb-12 xl:mb-0 px-4">
-          {/* <CountryStats /> */}
-        </div>
-        <div className="w-full xl:w-4/12 px-4">{/* <AsnPortStats /> */}</div>
-      </div>
-      <div className="flex flex-wrap mt-4">
-        <div className="w-full xl:w-8/12 mb-12 xl:mb-0 px-4">
-          {/* <CardPageVisits /> */}
-        </div>
-        <div className="w-full xl:w-4/12 px-4">
-          {/* <CardSocialTraffic /> */}
+    <div className="min-h-screen p-5 bg-gray-200 flex flex-col justify-center">
+      <div className="max-w-md min-h-1/2 w-full mx-auto">
+        <div className="">
+          <h4>Spectrum begins here</h4>
         </div>
       </div>
-    </>
+    </div>
   );
 }
