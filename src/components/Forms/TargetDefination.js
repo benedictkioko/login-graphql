061ce86fe0320e0ca48a7de478f1e609038a396d
benedict <kiokobenedict@gmail.com>
2021-03-13 22:24:47 +0300

    updated the class to className JSX elements

--- src/components/Forms/TargetDefination.js
+++ src/components/Forms/TargetDefination.js
@@ -4,59 +4,59 @@ import { Link } from "react-router-dom";
 export default function TargetDefination() {
   return (
     <>
-      <div class="w-full">
-        <h1 class="font-bold text-sm text-green-600 leading-loose my-3 pb-4">
+      <div className="w-full">
+        <h1 className="font-bold text-sm text-green-600 leading-loose my-3 pb-4">
           Add Target
         </h1>
-        <div class="items-center justify-center">
-          <div class="container bg-white rounded-md shadow-lg px-4 mr-4">
-            <div class="px-12 py-6">
-              <div class="text-center mb-4">
-                <div class="max-w-md items-center text-left py-4">
+        <div className="items-center justify-center">
+          <div className="container bg-white rounded-md shadow-lg px-4 mr-4">
+            <div className="px-12 py-6">
+              <div className="text-center mb-4">
+                <div className="max-w-md items-center text-left py-4">
                   <form action="#">
-                    <div class="-mx-3 md:flex mb-6">
-                      <div class="md:w-full px-3">
+                    <div className="-mx-3 md:flex mb-6">
+                      <div className="md:w-full px-3">
                         <label
-                          class="block uppercase tracking-wide text-grey-darker text-xs font-bold pb-4"
-                          for="grid-password"
+                          className="block uppercase tracking-wide text-grey-darker text-xs font-bold pb-4"
+                          htmlFor="grid-password"
                         >
                           Entity
                         </label>
                         <input
-                          class="appearance-none block w-full bg-grey-lighter text-grey-darker border border-grey-lighter rounded py-2 px-4 mb-3 focus:outline-none focus:bg-white focus:border-gray-500"
+                          className="appearance-none block w-full bg-grey-lighter text-grey-darker border border-grey-lighter rounded py-2 px-4 mb-3 focus:outline-none focus:bg-white focus:border-gray-500"
                           id="grid-password"
                           type="text"
                           placeholder=""
                         />
-                        <p class="text-grey-dark text-xs italic">
+                        <p className="text-grey-dark text-xs italic">
                           Entity name must be unique.
                         </p>
                       </div>
                     </div>
-                    <div class="-mx-3 md:flex  py-2">
-                      <div class="md:w-1/2 px-3  md:mb-0">
+                    <div className="-mx-3 md:flex  py-2">
+                      <div className="md:w-1/2 px-3  md:mb-0">
                         <label
-                          class="block uppercase tracking-wide text-grey-darker text-xs font-bold pb-4"
-                          for="grid-first-name"
+                          className="block uppercase tracking-wide text-grey-darker text-xs font-bold pb-4"
+                          htmlFor="grid-first-name"
                         >
                           Domain
                         </label>
                         <input
-                          class="appearance-none block w-full bg-grey-lighter text-grey-darker border border-red rounded py-2 px-4 mb-3 focus:outline-none focus:bg-white focus:border-gray-500"
+                          className="appearance-none block w-full bg-grey-lighter text-grey-darker border border-red rounded py-2 px-4 mb-3 focus:outline-none focus:bg-white focus:border-gray-500"
                           id="grid-first-name"
                           type="text"
                           placeholder="domain.com"
                         />
                       </div>
-                      <div class="md:w-1/2 px-3">
+                      <div className="md:w-1/2 px-3">
                         <label
-                          class="block uppercase tracking-wide text-grey-darker text-xs font-bold pb-4"
-                          for="grid-last-name"
+                          className="block uppercase tracking-wide text-grey-darker text-xs font-bold pb-4"
+                          htmlFor="grid-last-name"
                         >
                           IP Address
                         </label>
                         <input
-                          class="appearance-none block w-full bg-grey-lighter text-grey-darker border border-grey-lighter rounded py-2 px-4 focus:outline-none focus:bg-white focus:border-gray-500"
+                          className="appearance-none block w-full bg-grey-lighter text-grey-darker border border-grey-lighter rounded py-2 px-4 focus:outline-none focus:bg-white focus:border-gray-500"
                           id="grid-last-name"
                           type="text"
                           placeholder="192.168.0.1"
@@ -64,24 +64,24 @@ export default function TargetDefination() {
                       </div>
                     </div>
                     {/* sector */}
-                    <div class="-mx-3 md:flex mb-6">
-                      <div class="md:w-full px-3">
+                    <div className="-mx-3 md:flex mb-6">
+                      <div className="md:w-full px-3">
                         <label
-                          class="block uppercase tracking-wide text-grey-darker text-xs font-bold pb-4"
-                          for="grid-state"
+                          className="block uppercase tracking-wide text-grey-darker text-xs font-bold pb-4"
+                          htmlFor="grid-state"
                         >
                           Sector
                         </label>
-                        <div class="relative">
-                          <select class="appearance-none h-full rounded-l border block w-full bg-white border-gray-400 text-gray-700 py-2 px-4 pr-8 leading-tight focus:outline-none focus:bg-white focus:border-gray-500">
+                        <div className="relative">
+                          <select className="appearance-none h-full rounded-l border block w-full bg-white border-gray-400 text-gray-700 py-2 px-4 pr-8 leading-tight focus:outline-none focus:bg-white focus:border-gray-500">
                             <option>-Choose-</option>
                             <option>Agriculture</option>
                             <option>Banking</option>
                             <option>ICT</option>
                           </select>
-                          <div class="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 m-4 text-gray-700">
+                          <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 m-4 text-gray-700">
                             <svg
-                              class="fill-current h-4 w-4"
+                              className="fill-current h-4 w-4"
                               xmlns="http://www.w3.org/2000/svg"
                               viewBox="0 0 20 20"
                             >
@@ -92,42 +92,39 @@ export default function TargetDefination() {
                       </div>
                     </div>
                     {/* country */}
-                    <div class="-mx-3 md:flex mb-2">
-                      <div class="md:w-1/2 px-3 mb-6 md:mb-0">
+                    <div className="-mx-3 md:flex mb-2">
+                      <div className="md:w-1/2 px-3 mb-6 md:mb-0">
                         <label
-                          class="block uppercase tracking-wide text-grey-darker text-xs font-bold mb-2 pb-4"
-                          for="grid-city"
+                          className="block uppercase tracking-wide text-grey-darker text-xs font-bold mb-2 pb-4"
+                          htmlFor="grid-city"
                         >
                           City
                         </label>
                         <input
-                          class="appearance-none block w-full bg-grey-lighter text-grey-darker border border-grey-lighter rounded py-2 px-4"
+                          className="appearance-none block w-full bg-grey-lighter text-grey-darker border border-grey-lighter rounded py-2 px-4"
                           id="grid-city"
                           type="text"
                           placeholder="Nairobi"
                         />
                       </div>
 
-                      <div class="md:w-1/2 px-3">
+                      <div className="md:w-1/2 px-3">
                         <label
-                          class="block uppercase tracking-wide text-grey-darker text-xs font-bold mb-2 pb-4"
-                          for="grid-state"
+                          className="block uppercase tracking-wide text-grey-darker text-xs font-bold mb-2 pb-4"
+                          htmlFor="grid-state"
                         >
                           Country
                         </label>
-                        <div class="relative">
-                          <select
-                            class="block appearance-none w-full bg-grey-lighter border border-grey-lighter text-grey-darker py-2 px-4 pr-8 rounded"
-                            id="grid-state"
-                          >
+                        <div className="relative">
+                          <select className="appearance-none h-full rounded-l border block w-full bg-white border-gray-400 text-gray-700 py-2 px-4 pr-8 leading-tight focus:outline-none focus:bg-white focus:border-gray-500">
                             <option>-Choose-</option>
                             <option>Uganda</option>
                             <option>Kenya</option>
                             <option>Somalia</option>
                           </select>
-                          <div class="pointer-events-none absolute pin-y pin-r flex items-center px-2 text-grey-darker">
+                          <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 m-4 text-gray-700">
                             <svg
-                              class="h-4 w-4"
+                              className="fill-current h-4 w-4"
                               xmlns="http://www.w3.org/2000/svg"
                               viewBox="0 0 20 20"
                             >
@@ -137,17 +134,17 @@ export default function TargetDefination() {
                         </div>
                       </div>
                     </div>
-                    <div class="-mx-3 md:w-full  py-4">
-                      <div class="md:w-1/2 px-3  md:mb-0">
+                    <div className="-mx-3 md:flex  py-4">
+                      <div className="md:w-full px-3">
                         <label
-                          class="block uppercase tracking-wide text-grey-darker text-xs font-bold mb-2 pb-4"
-                          for="grid-password"
+                          className="block uppercase tracking-wide text-grey-darker text-xs font-bold mb-2 pb-4"
+                          htmlFor="grid-password"
                         >
                           Notes
                         </label>
 
                         <textarea
-                          class="autoexpand tracking-wide py-2 px-4 mb-3 leading-relaxed appearance-none block w-full bg-gray-200 border border-gray-200 rounded focus:outline-none focus:bg-white focus:border-gray-500"
+                          className="autoexpand tracking-wide py-2 px-4 mb-3 leading-relaxed appearance-none block w-full bg-gray-200 border border-gray-200 rounded focus:outline-none focus:bg-white focus:border-gray-500"
                           id="message"
                           type="text"
                           placeholder="Notes..."
@@ -159,7 +156,7 @@ export default function TargetDefination() {
                       <div className="md:w-1/2 px-3  md:mb-0">
                         <button
                           type="submit"
-                          class="appearance-none bg-red-600 text-white hover:bg-red-600 text-base font-semibold tracking-wide uppercase p-2 rounded shadow hover:bg-indigo-light"
+                          className="appearance-none bg-red-600 text-white hover:bg-red-600 text-base font-semibold tracking-wide uppercase p-2 rounded shadow hover:bg-indigo-light"
                         >
                           Cancel
                         </button>
@@ -167,7 +164,7 @@ export default function TargetDefination() {
                       <div className="md:w-1/2 px-3  md:mb-0">
                         <button
                           type="submit"
-                          class="appearance-none bg-gray-900 text-white hover:bg-gray-600 text-base font-semibold tracking-wide uppercase p-2 rounded shadow hover:bg-indigo-light"
+                          className="appearance-none bg-gray-900 text-white hover:bg-gray-600 text-base font-semibold tracking-wide uppercase p-2 rounded shadow hover:bg-indigo-light"
                         >
                           Submit
                         </button>
