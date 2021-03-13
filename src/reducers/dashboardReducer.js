1be5297802d5c3cdf92ddd988fc08bd1c1778ddb
benedict <kiokobenedict@gmail.com>
2021-03-13 22:23:29 +0300

    updated the dashboardReducer Initial state values to nill

--- src/reducers/dashboardReducer.js
+++ src/reducers/dashboardReducer.js
@@ -1,10 +1,10 @@
 import { dashboard } from "../actions/actionTypes";
 
 const initialState = {
-  totalCountries: 11,
-  totalTargets: 12361,
-  totalDomains: 92705,
-  totalServices: 398,
+  totalCountries: "",
+  totalTargets: "",
+  totalDomains: "",
+  totalServices: "",
 };
 
 export default function dashboardReducer(state = initialState, action) {
@@ -12,7 +12,7 @@ export default function dashboardReducer(state = initialState, action) {
     case dashboard.GET_STATS_SUCCESS:
       const State = {
         ...state,
-        search: action.payload,
+        ...action.payload,
       };
       return State;
     default:
