5daab5e95cae669b1df34eacd26808a36d4d482c
benedict <kiokobenedict@gmail.com>
2021-03-13 22:25:36 +0300

    updated the fill-rule and clip-rule to react  elements

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -20,16 +20,16 @@ export default function Navbar() {
           </Link>
 
           <svg
-            class="flex-shrink-0 h-5 w-5 text-gray-400"
+            className="flex-shrink-0 h-5 w-5 text-gray-400"
             xmlns="http://www.w3.org/2000/svg"
             viewBox="0 0 20 20"
             fill="currentColor"
             aria-hidden="true"
           >
             <path
-              fill-rule="evenodd"
+              fillRule="evenodd"
               d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
-              clip-rule="evenodd"
+              clipRule="evenodd"
             />
           </svg>
           <span className="text-white text-sm uppercase hidden lg:inline-block font-semibold">
