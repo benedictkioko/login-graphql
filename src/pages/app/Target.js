d4b3beb2fa8c429e05989651b45837af770a25df
benedict <kiokobenedict@gmail.com>
2021-03-22 23:27:37 +0300

    WIP-fixed .slice is not a function on initail load when state is empty

--- src/pages/app/Target.js
+++ src/pages/app/Target.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
 import { useLazyQuery } from "@apollo/client";
 import { useDispatch, shallowEqual, useSelector } from "react-redux";
 import ReactPaginate from "react-paginate";
@@ -19,14 +19,15 @@ function Target() {
   const dispatch = useDispatch();
 
   // global state
-  const state = useSelector((state) => state.targets, shallowEqual);
+  const targetState = useSelector((state) => state.targets, shallowEqual);
 
-  const [pageNumber, setPageNumber] = useState(0);
-
+  let [pageNumber, setPageNumber] = useState(1);
+  let displayTargets = useRef([]);
+  let pageCount = useRef(1);
   const targetPerPage = 10;
   const pagesVisted = pageNumber * targetPerPage;
-
-  const [allTargets, { data, loading, error }] = useLazyQuery(GET_TARGETS, {
+  let [targetLoaded, setTargetLoaded] = useState(false);
+  let [allTargets, { data, loading, error }] = useLazyQuery(GET_TARGETS, {
     variables: {
       search: "",
       n: 100,
@@ -42,16 +43,48 @@ function Target() {
     allTargets();
   }, [allTargets]);
 
-  const displayTargets = state.allTargets
-    .slice(pagesVisted, pagesVisted + targetPerPage)
-    .map((target) => {
-      return <Body key={target.id} data={target} />;
-    });
-
-  const pageCount = Math.ceil(state.allTargets.length / targetPerPage);
+  useEffect(() => {
+    if (
+      Array.isArray(targetState.allTargets) &&
+      !targetLoaded &&
+      targetState.allTargets.length > 0
+    ) {
+      // targetState.allTargets.sort(function (a, b) {
+      //   var nameA = a.name.toUpperCase(); // ignore upper and lowercase
+      //   var nameB = b.name.toUpperCase(); // ignore upper and lowercase
+      //   if (nameA < nameB) {
+      //     return -1;
+      //   }
+      //   if (nameA > nameB) {
+      //     return 1;
+      //   }
+      //   // names must be equal
+      //   return 0;
+      // });
+      displayTargets.current = targetState.allTargets
+        .slice(pagesVisted, pagesVisted + targetPerPage)
+        .map((target) => {
+          return <Body key={target.id} data={target} />;
+        });
+      pageCount.current = Math.ceil(
+        targetState.allTargets.length / targetPerPage
+      );
+      setTargetLoaded(true);
+      console.log("TARGETS: ", displayTargets.current);
+      console.log("PAGE COUNT: ", pageCount.current);
+    }
+  }, [targetState.allTargets, targetLoaded, pageNumber, pagesVisted]);
 
   const changePage = ({ selected }) => {
     setPageNumber(selected);
+    displayTargets.current = targetLoaded
+      ? targetState.allTargets.length > 0 &&
+        targetState.allTargets
+          .slice(pagesVisted, pagesVisted + targetPerPage)
+          .map((target) => {
+            return <Body key={target.id} data={target} />;
+          })
+      : [...displayTargets.current];
   };
 
   return (
@@ -61,7 +94,6 @@ function Target() {
           <div className="items-center">
             {/* Search Form */}
             <Search />
-            {loading && <div>Loading...</div>}
             {/* Search Results Table */}
             <div className="flex flex-col">
               <h1 className="font-bold text-sm text-green-600 pb-4">
@@ -69,41 +101,46 @@ function Target() {
               </h1>
 
               <div className="shadow overflow-hidden overflow-x-auto border-b border-gray-200 sm:rounded-lg">
-                {/* table */}
                 {/* <SearchResults data={targets} /> */}
-                <table className="w-full divide-y divide-gray-200">
-                  <Header />
-                  {displayTargets}
-                </table>
-                {/* Pagination */}
-                <div className="px-5 py-5 border-t flex flex-col sm:hidden xs:flex-row items-center xs:justify-between mt-4 mb-6">
-                  <div className="inline-flex mt-2 xs:mt-0">
-                    <ReactPaginate
-                      previousLabel={"Previous page"}
-                      nextLabel={"Next page"}
-                      pageCount={pageCount}
-                      onPageChange={changePage}
-                      containerClassName={
-                        "flex rounded-sm font-bold py-2 px-2 mx-4 items-center"
-                      }
-                      previousLinkClassName={
-                        "border border-teal-500 text-teal-500 block rounded-sm font-bold py-2 px-4 flex items-center hover:bg-teal-500 hover:text-white"
-                      }
-                      nextLinkClassName={
-                        "border border-teal-500 text-teal-500 block rounded-sm font-bold py-2 px-4 flex items-center hover:bg-teal-500 hover:text-white"
-                      }
-                      disabledClassName={
-                        "disabled mx-2 bg-teal-200 text-white hover:text-white"
-                      }
-                      pageLinkClassName={
-                        "border border-teal-500 text-gray-900 block rounded-sm font-normal py-2 px-4 mx-2 flex items-center hover:bg-teal-500 hover:text-white"
-                      }
-                      activeLinkClassName={
-                        "border border-teal-500 bg-teal-500 text-white block rounded-sm font-bold py-2 px-2 ml-2"
-                      }
-                    />
-                  </div>
-                </div>
+                {targetLoaded ? (
+                  <>
+                    <table className="w-full divide-y divide-gray-200">
+                      <Header />
+                      {displayTargets.current}
+                    </table>
+                    {/* pagination */}
+                    <div className="px-5 py-5 border-t flex flex-col xs:flex-row items-center xs:justify-between mt-4 mb-6">
+                      <div className="inline-flex mt-2 xs:mt-0">
+                        <ReactPaginate
+                          previousLabel={"Previous page"}
+                          nextLabel={"Next page"}
+                          pageCount={pageCount.current}
+                          onPageChange={changePage}
+                          containerClassName={
+                            "flex rounded-sm font-bold py-2 px-2 mx-4 items-center"
+                          }
+                          previousLinkClassName={
+                            "border border-teal-500 text-teal-500 block rounded-sm font-bold py-2 px-4 flex items-center hover:bg-teal-500 hover:text-white"
+                          }
+                          nextLinkClassName={
+                            "border border-teal-500 text-teal-500 block rounded-sm font-bold py-2 px-4 flex items-center hover:bg-teal-500 hover:text-white"
+                          }
+                          disabledClassName={
+                            "appearance-none mx-2 bg-gray-300 text-white hover:text-white"
+                          }
+                          pageLinkClassName={
+                            "border border-teal-500 text-gray-900 block rounded-sm font-normal py-2 px-4 mx-2 flex items-center hover:bg-teal-500 hover:text-white"
+                          }
+                          activeLinkClassName={
+                            "border border-teal-500 bg-teal-500 text-white block rounded-sm font-bold py-2 px-2 ml-2"
+                          }
+                        />
+                      </div>
+                    </div>
+                  </>
+                ) : (
+                  <div>Loading...</div>
+                )}
               </div>
             </div>
             <div className="items-center mt-4">
