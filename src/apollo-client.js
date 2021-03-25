94a6521baec8425833716be4f4ab16d30de59079
benedict <kiokobenedict@gmail.com>
2021-03-25 20:15:05 +0300

    added arroeLink to log any GraphQL errors or network error that occurred

--- src/apollo-client.js
+++ src/apollo-client.js
@@ -5,8 +5,20 @@ import {
   InMemoryCache,
   concat,
 } from "@apollo/client";
+import { onError } from "@apollo/client/link/error";
 
 const httpLink = new HttpLink({ uri: "http://localhost:8000/api/v1/" });
+
+// Log any GraphQL errors or network error that occurred
+const errorLink = onError(({ graphQLErrors, networkError }) => {
+  if (graphQLErrors)
+    graphQLErrors.map(({ message, locations, path }) =>
+      console.log(
+        `[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`
+      )
+    );
+  if (networkError) console.log(`[Network error]: ${networkError}`);
+});
 
 const authMiddleware = new ApolloLink((operation, forward) => {
   const token = localStorage.getItem("state");
@@ -25,8 +37,9 @@ const authMiddleware = new ApolloLink((operation, forward) => {
 
 const client = new ApolloClient({
   cache: new InMemoryCache(),
-  link: concat(authMiddleware, httpLink),
+  link: errorLink.concat(concat(authMiddleware, httpLink)),
   credentials: "include",
+  onError: (error) => error,
 });
 
 export { client };
