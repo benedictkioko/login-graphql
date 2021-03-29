9c20da430d3dd5e8034a455f00a423889bca5057
benedict <kiokobenedict@gmail.com>
2021-03-29 23:07:21 +0300

    added the errorReducer to the rootReducer

--- src/redux/rootReducer.js
+++ src/redux/rootReducer.js
@@ -2,6 +2,7 @@ import { combineReducers } from "redux";
 import authReducer from "../reducers/authReducer";
 import dashboardReducer from "../reducers/dashboardReducer";
 import targetReducer from "../reducers/targetReducer";
+import errorReducer from "../reducers/errorReducer";
 import storage from "redux-persist/lib/storage";
 
 const rootReducer = combineReducers({
@@ -9,6 +10,7 @@ const rootReducer = combineReducers({
   auth: authReducer,
   dashboard: dashboardReducer,
   targets: targetReducer,
+  error: errorReducer,
   storage: storage,
 });
 
