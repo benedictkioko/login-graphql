1632a5c7a17f906fabf5865da52ce8ee8c15488b
benedict <kiokobenedict@gmail.com>
2021-03-31 10:39:48 +0300

    added a placeholder Loader eith threeDots

--- src/pages/app/Stats.js
+++ src/pages/app/Stats.js
@@ -1,11 +1,12 @@
 import React from "react";
+import Loader from "react-loader-spinner";
 
 export default function Stats() {
   return (
     <div className="min-h-screen p-5 bg-gray-200 flex flex-col justify-center">
       <div className="max-w-md min-h-1/2 w-full mx-auto">
         <div className="">
-          <h4>Stats begins here</h4>
+          <Loader type="ThreeDots" />
         </div>
       </div>
     </div>
