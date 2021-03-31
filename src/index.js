ba8a08eae0740c84f65f2f866d693219f5688161
benedict <kiokobenedict@gmail.com>
2021-03-31 10:44:03 +0300

    added a Toast Container - temp

--- src/index.js
+++ src/index.js
@@ -5,11 +5,17 @@ import App from "./App";
 import { client } from "./apollo-client";
 import { Provider } from "react-redux";
 import store from "./redux/store";
+import { ToastContainer } from "react-toast";
 
 ReactDOM.render(
   <ApolloProvider client={client}>
     <Provider store={store}>
       <App />
+      <ToastContainer
+        delay={5000}
+        position="top-right"
+        style={{ "padding-top": "2vh" }}
+      />
     </Provider>
   </ApolloProvider>,
   document.getElementById("root")
