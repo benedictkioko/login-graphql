706eef1ecbeeea55e8f1b0b77dbb12a250bd7521
benedict <kiokobenedict@gmail.com>
2021-04-07 21:25:37 +0300

    modified the social traffic card to targets per country

--- src/components/Cards/AsnPortStats.js
+++ src/components/Cards/AsnPortStats.js
@@ -2,6 +2,14 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 // components
+// title case
+function titleCase(str) {
+  str = str.toLowerCase().split(" ");
+  for (var i = 0; i < str.length; i++) {
+    str[i] = str[i].charAt(0).toUpperCase() + str[i].slice(1);
+  }
+  return str.join(" ");
+}
 
 export default function CardSocialTraffic() {
   return (
@@ -11,17 +19,17 @@ export default function CardSocialTraffic() {
           <div className="flex flex-wrap items-center">
             <div className="relative w-full px-4 max-w-full flex-grow flex-1">
               <h3 className="font-semibold text-base text-gray-800">
-                Social traffic
+                Entities per Country
               </h3>
             </div>
-            <div className="relative w-full px-4 py-3 max-w-full flex-grow flex-1 text-right">
+            {/* <div className="relative w-full px-4 py-3 max-w-full flex-grow flex-1 text-right">
               <Link
                 to="/app/target"
                 className="mt-4 px-4 py-2 border-b-2 border-blue-400 font-bold text-gray-500 hover:border-red-500"
               >
                 <i className="fas fa-eye bg-transparent mr-2 text-sm"> view</i>
               </Link>
-            </div>
+            </div> */}
           </div>
         </div>
         <div className="block w-full overflow-x-auto">
@@ -30,29 +38,35 @@ export default function CardSocialTraffic() {
             <thead className="thead-light">
               <tr>
                 <th className="px-6 bg-gray-100 text-gray-600 align-middle border border-solid border-gray-200 py-3 text-xs uppercase border-l-0 border-r-0 whitespace-no-wrap font-semibold text-left">
-                  Referral
+                  Country
                 </th>
                 <th className="px-6 bg-gray-100 text-gray-600 align-middle border border-solid border-gray-200 py-3 text-xs uppercase border-l-0 border-r-0 whitespace-no-wrap font-semibold text-left">
-                  Visitors
+                  Total
                 </th>
-                <th className="px-6 bg-gray-100 text-gray-600 align-middle border border-solid border-gray-200 py-3 text-xs uppercase border-l-0 border-r-0 whitespace-no-wrap font-semibold text-left min-w-140-px"></th>
+                <th className="px-6 bg-gray-100 text-gray-600 align-middle border border-solid border-gray-200 py-3 text-xs uppercase border-l-0 border-r-0 whitespace-no-wrap font-semibold text-left min-w-140-px">
+                  %
+                </th>
               </tr>
             </thead>
             <tbody>
               <tr>
                 <th className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-no-wrap p-4 text-left">
-                  Facebook
+                  Kenya
                 </th>
                 <td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-no-wrap p-4">
-                  1,480
+                  {(12332).toLocaleString()}
                 </td>
                 <td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-no-wrap p-4">
                   <div className="flex items-center">
-                    <span className="mr-2">60%</span>
+                    <span className="mr-2">
+                      {((12332 / 12365) * 100).toFixed(2)}%
+                    </span>
                     <div className="relative w-full">
                       <div className="overflow-hidden h-2 text-xs flex rounded bg-red-200">
                         <div
-                          style={{ width: "60%" }}
+                          style={{
+                            width: `${((12332 / 12365) * 100).toFixed(2)}%`,
+                          }}
                           className="shadow-none flex flex-col text-center whitespace-nowrap text-white justify-center bg-red-500"
                         ></div>
                       </div>
@@ -62,18 +76,22 @@ export default function CardSocialTraffic() {
               </tr>
               <tr>
                 <th className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-no-wrap p-4 text-left">
-                  Facebook
+                  {titleCase("SOMALIA")}
                 </th>
                 <td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-no-wrap p-4">
-                  5,480
+                  7
                 </td>
                 <td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-no-wrap p-4">
                   <div className="flex items-center">
-                    <span className="mr-2">70%</span>
+                    <span className="mr-2">
+                      {((7 / 12365) * 100).toFixed(2)}%
+                    </span>
                     <div className="relative w-full">
                       <div className="overflow-hidden h-2 text-xs flex rounded bg-green-200">
                         <div
-                          style={{ width: "70%" }}
+                          style={{
+                            width: `${((7 / 12365) * 100).toFixed(2)}%`,
+                          }}
                           className="shadow-none flex flex-col text-center whitespace-nowrap text-white justify-center bg-green-500"
                         ></div>
                       </div>
@@ -83,18 +101,22 @@ export default function CardSocialTraffic() {
               </tr>
               <tr>
                 <th className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-no-wrap p-4 text-left">
-                  Google
+                  {titleCase("Uganda")}
                 </th>
                 <td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-no-wrap p-4">
-                  4,807
+                  6
                 </td>
                 <td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-no-wrap p-4">
                   <div className="flex items-center">
-                    <span className="mr-2">80%</span>
+                    <span className="mr-2">
+                      {((6 / 12365) * 100).toFixed(2)}%
+                    </span>
                     <div className="relative w-full">
                       <div className="overflow-hidden h-2 text-xs flex rounded bg-purple-200">
                         <div
-                          style={{ width: "80%" }}
+                          style={{
+                            width: `${((6 / 12365) * 100).toFixed(2)}%`,
+                          }}
                           className="shadow-none flex flex-col text-center whitespace-nowrap text-white justify-center bg-purple-500"
                         ></div>
                       </div>
@@ -104,18 +126,22 @@ export default function CardSocialTraffic() {
               </tr>
               <tr>
                 <th className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-no-wrap p-4 text-left">
-                  Instagram
+                  {titleCase("SUDAN")}
                 </th>
                 <td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-no-wrap p-4">
-                  3,678
+                  3
                 </td>
                 <td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-no-wrap p-4">
                   <div className="flex items-center">
-                    <span className="mr-2">75%</span>
+                    <span className="mr-2">
+                      {((3 / 12365) * 100).toFixed(2)}%
+                    </span>
                     <div className="relative w-full">
                       <div className="overflow-hidden h-2 text-xs flex rounded bg-blue-200">
                         <div
-                          style={{ width: "75%" }}
+                          style={{
+                            width: `${((3 / 12365) * 100).toFixed(2)}%`,
+                          }}
                           className="shadow-none flex flex-col text-center whitespace-nowrap text-white justify-center bg-blue-500"
                         ></div>
                       </div>
@@ -125,18 +151,22 @@ export default function CardSocialTraffic() {
               </tr>
               <tr>
                 <th className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-no-wrap p-4 text-left">
-                  twitter
+                  {titleCase("RWANDA")}
                 </th>
                 <td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-no-wrap p-4">
-                  2,645
+                  2
                 </td>
                 <td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-no-wrap p-4">
                   <div className="flex items-center">
-                    <span className="mr-2">30%</span>
+                    <span className="mr-2">
+                      {((2 / 12365) * 100).toFixed(2)}%
+                    </span>
                     <div className="relative w-full">
                       <div className="overflow-hidden h-2 text-xs flex rounded bg-orange-200">
                         <div
-                          style={{ width: "30%" }}
+                          style={{
+                            width: `${((2 / 12365) * 100).toFixed(2)}%`,
+                          }}
                           className="shadow-none flex flex-col text-center whitespace-nowrap text-white justify-center bg-green-500"
                         ></div>
                       </div>
